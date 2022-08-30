57f8f9d52914517a28796c71700a12dd26b5a858
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-08-30 20:43:02 +0900

    named arguments sample.

--- grammer/named_argments.dart
+++ grammer/named_argments.dart
@@ -10,8 +10,13 @@ bool equal3({required first, required second}) {
   return first == second;
 }
 
+bool equal4({required first, second = 2}) {
+  return first == second;
+}
+
 void main() {
   print(equal(first: 1));
   print(equal2(second: 2));
   print(equal3(second: 2, first: 2));
+  print(equal4(first: 2));
 }
